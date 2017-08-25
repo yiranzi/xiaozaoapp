aa617a8edae326d063f258023da63cc90010cf04
wangyuanli <yuanli.wang@xiaozao.org>
2017-08-25 14:50:54 +0800

    chore: 引入样式库weui

--- pages/school/center.js
+++ pages/school/center.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Layout from '../../src/components/layout';
-import {DatePicker} from 'antd';
+
+import { Button } from 'react-weui';
 
 export default class extends React.Component {
     render() {
         return (
             <Layout>
-                <DatePicker/>
+                <Button>阿萨德法师打发</Button>
             </Layout>
         );
     }
