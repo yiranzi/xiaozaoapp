7b874f4d374ad3cf73df3e7c3a101b42a1631475
wangyuanli <yuanli.wang@xiaozao.org>
2017-08-31 14:59:24 +0800

    feature: 今日做题流程

--- src/page/writtentestclock/answer/index.js
+++ src/page/writtentestclock/answer/index.js
@@ -1,27 +1,139 @@
 import React from 'react';
-import SubjectComponent from './components/subject';
-import ThemeConfig from '../../../config/theme';
+import classNames from 'classnames';
+import SubjectComponent from '../components/subject';
+import ThemeConfig from '../../../../config/theme';
 
 export default class AnswerPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentObjectIndex: 0
+            currentObjectIndex: 0,
+            answerList: {},
+            finish: false,
+            isShowAnalysis: false
         };
     }
 
-    renderAnswer() {
-        const {currentObjectIndex} = this.state; //当前题目序号
-        const {questions} = this.props;
+    renderAnswer(currentObjectIndex, questions) {
+        const {answerList} = this.state;
+
         const questionItem = questions[currentObjectIndex];//题目详情
+        const subjectItem = Object.assign({}, {
+            total: questions.length,//当前试卷总共多少题
+            currentIndex: currentObjectIndex, //当前题目在数组中的编号
+            questionItem: questionItem, //题目数组
+            selectAnswer: answerList[questionItem.id],//已选答案
+        })
+        return (
+            <div className='subject-item'>
+                <SubjectComponent
+                    subjectItem={subjectItem}
+                    onChange={(value) => {
+                        this.answerCheck(questionItem.id, value);
+                    }}
+                />
+            </div>
+        );
+    }
+
+    answerCheck(id, value) {
+        const {answerList} = this.state;
+        answerList[id] = value;
+    }
+
+    prevAnswer(currentObjectIndex) {
+        this.setState({
+            currentObjectIndex: currentObjectIndex - 1,
+            isShowAnalysis: false
+        });
+    }
+
+    nextAnswer(currentObjectIndex, questions) {
+        let nextObjectIndex = currentObjectIndex + 1;
+        if (nextObjectIndex >= questions.length - 1) {
+            this.setState({
+                currentObjectIndex: nextObjectIndex,
+                finish: true,
+                isShowAnalysis: false
+            });
+        } else {
+            this.setState({
+                currentObjectIndex: nextObjectIndex,
+                isShowAnalysis: false
+            });
+        }
+    }
+
+    renderAnswerAnalysis(currentObjectIndex, questions) {
+        const {isShowAnalysis} = this.state;
+        const questionItem = questions[currentObjectIndex];
+        const {answer, analysis} = questionItem;
+
+        const analysisContent = (
+            <div className="wrapper">
+                <div className="analysis-header">
+                    <div className="answer">答案：{answer}</div>
+                    <div className="line"></div>
+                    <div className="hide-analysis">查看解析</div>
+                </div>
+                <div className="analysis-content">{analysis}</div>
+                <style jsx>{`
+                    .analysis-header {
+                        display: flex;
+                        justify-content: space-between;
+                    }
+                    .hide-analysis {
+                        display: inline-block;
+                        border: 1px solid ${ThemeConfig.color.writtentestclockmain};
+                        font-size: ${ThemeConfig.size.normal};
+                        font-weight: bold;
+                        border-radius: 1rem;
+                        padding: 0.1rem 0.5rem;
+                    }
+                `}</style>
+            </div>
+        )
 
         return (
-            <div className="subject-item">
-                <SubjectComponent
-                    subject={questionItem}
-                    index={currentObjectIndex}
-                    total={questions.length}
-                />
+            <div className="analysis">
+                {isShowAnalysis ? analysisContent : <div onClick={() => {
+                    this.showAnalysis(isShowAnalysis);
+                }} className="show-analysis">查看解析</div>}
+                <style jsx>{`
+                    .show-analysis {
+                        display: inline-block;
+                        border: 1px solid ${ThemeConfig.color.writtentestclockmain};
+                        font-size: ${ThemeConfig.size.normal};
+                        font-weight: bold;
+                        border-radius: 1rem;
+                        padding: 0.1rem 0.5rem;
+                    }
+                `}</style>
+            </div>
+        );
+    }
+
+    showAnalysis(isShowAnalysis) {
+        this.setState({isShowAnalysis: !isShowAnalysis});
+    }
+
+    renderActionButton(currentObjectIndex, questions) {
+        return (
+            <div className='action'>
+                <div onClick={() => {
+                    this.prevAnswer(currentObjectIndex);
+                }}><img src='/static/prev.png'/></div>
+                <div onClick={() => {
+                    this.nextAnswer(currentObjectIndex, questions);
+                }}><img src='/static/next.png'/></div>
+            </div>
+        );
+    }
+
+    renderFinishButton() {
+        return (
+            <div className="finish">
+                <div><img src="/static/complete-test.png"/></div>
             </div>
         );
     }
@@ -43,18 +155,22 @@ export default class AnswerPage extends React.Component {
                 .action img {
                     width: 100%;
                 }
+                .finish {
+                    display: flex;
+                    justify-content: center;
+                }
             `}</style>
         );
     }
 
     render() {
+        const {currentObjectIndex, finish} = this.state;//当前题目在数组中的序号
+        const {questions} = this.props;
         return (
-            <div className="written-test-clock-answer">
-                {this.renderAnswer()}
-                <div className="action">
-                    <div><img src="/static/prev.png"/></div>
-                    <div><img src="/static/next.png"/></div>
-                </div>
+            <div className='written-test-clock-answer'>
+                {this.renderAnswer(currentObjectIndex, questions)}
+                {this.renderAnswerAnalysis(currentObjectIndex, questions)}
+                {finish ? this.renderFinishButton() : this.renderActionButton(currentObjectIndex, questions)}
                 {this.renderCss()}
             </div>
         );
