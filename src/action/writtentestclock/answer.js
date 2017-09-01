f2c3a1f52d514ef75f988544c5c5da626317a00a
wangyuanli <yuanli.wang@xiaozao.org>
2017-09-01 17:33:32 +0800

    feature: 题目接口

--- src/action/writtentestclock/answer.js
+++ src/action/writtentestclock/answer.js
@@ -1,11 +1,18 @@
 const AxiosUtil = require('../../../src/util/axios');
 
 const AnswerAction = {
-    //获取试题
+    //获取测评
     getEvaluation: function () {
         return AxiosUtil({
             method: 'get',
             url: '/api/clock/getEvaluation'
+        });
+    },
+    //获取测试题目
+    getTest: function () {
+        return AxiosUtil({
+            method: 'get',
+            url: '/api/clock/getTest'
         });
     },
     //获取今日推送题目
@@ -22,6 +29,14 @@ const AnswerAction = {
             url: '/api/clock/getYesterday'
         });
     },
+    //获取指定做题记录
+    getByToday: function (day) {
+        return AxiosUtil({
+            method: 'get',
+            url: `/api/clock/getByDay?day=${day}`
+        });
+    },
+
     //提交做题记录
     complete: function (data) {
         return AxiosUtil({
