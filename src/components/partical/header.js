cb662cee126f3bdc4eaf2d47947d4767c194cf0a
wangyuanli <yuanli.wang@xiaozao.org>
2017-09-04 20:54:28 +0800

    update: 更新title

--- src/components/partical/header.js
+++ src/components/partical/header.js
@@ -8,6 +8,7 @@ export default class Header extends React.Component {
                 <Head>
                     <meta charSet="UTF-8"/>
                     <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
+                    <title>小灶求职</title>
                 </Head>
             </div>
         );
