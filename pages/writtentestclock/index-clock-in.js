0d0fec5b6ff7b7a4467a5a2b8e8e480a08c8621b
leonzhang <human1108@163.com>
2017-09-04 14:33:40 +0800

    update: index-clock-in修改

--- pages/writtentestclock/index-clock-in.js
+++ pages/writtentestclock/index-clock-in.js
@@ -2,24 +2,12 @@ import React from 'react';
 import {Button} from 'react-weui';
 import IndexClockIn from '../../src/page/writtentestclock/index-clock-in'
 import WrittenTestClock from '../../src/page/writtentestclock/components/layout';
-import UserAction from '../../src/action/writtentestclock/user';
 
 export default class extends React.Component {
-    static async getInitialProps() {
-        let info;
-        try {
-            info = await UserAction.getYesterday();
-        } catch (error) {
-            info = error
-        }
-        return {
-            info
-        }
-    }
     render() {
         return (
             <WrittenTestClock>
-                <IndexClockIn {...this.props}/>
+                <IndexClockIn/>
             </WrittenTestClock>
         );
     }
