69ef269c87742c96f52c49a8a896b434e29c46d5
wangyuanli <yuanli.wang@xiaozao.org>
2017-09-05 16:45:36 +0800

    update: 去掉分享

--- src/page/writtentestclock/test-result/index.js
+++ src/page/writtentestclock/test-result/index.js
@@ -31,33 +31,38 @@ export default class TestResultPage extends React.Component {
     }
 
     render() {
-        if(!this.state.showPage) return <div></div>
+        if (!this.state.showPage) return <div></div>
         const {testInfo} = this.state;
         const content = this.state.testInfo.totalScore >= 65 ? '实力总是需要有人见证的！' : '从这一刻开始改变！'
         const accuracy = !testInfo ? 0 : (!testInfo.writtenTestTopicDTOList || testInfo.writtenTestTopicDTOList.length == 0) ? 0 : Math.round(testInfo.totalScore / testInfo.writtenTestTopicDTOList.length * 100);
         return (
-            <div>
-                <ShareWx isShow={this.state.shareIsShow}/>
+            <div className="test-result-page">
                 <div className="test-result">
                     <img className="result-bg" src="/static/writtentestclock/test_result_bg.png"/>
                     <div className="result-content">
-                        <div className="text1">恭喜您本次测试答对 {this.state.testInfo ? this.state.testInfo.totalScore : 0} 道题<br/>总正确率{accuracy}%！</div>
+                        <div className="text1">恭喜您本次测试答对 {this.state.testInfo ? this.state.testInfo.totalScore : 0} 道题<br/>总正确率{accuracy}%！
+                        </div>
                         <div className="text2">小伙伴，你的笔试答题战斗力<br/>超过了{this.state.exceeds[accuracy]}%的笔试打卡学习者！</div>
-                        <a className='btn1' href="javascript:;" onClick={this.shareWx}><img src="/static/writtentestclock/share.png"/></a>
+                        {/*<a className='btn1' href="javascript:;" onClick={this.shareWx}><img src="/static/writtentestclock/share.png"/></a>*/}
                         <div className="text2">{content}<br/>少侠，笔试训练也是一个打怪升级的过程</div>
                         <br/>
                         <div className="text2"><span className="small">//////////</span> 您目前最适合进入 <span className="small">//////////</span><br/>
                             全能提升基础班
                         </div>
-                        <a className='btn2' href="/writtentestclock/clock-in-intro"><img src="/static/writtentestclock/start_writtentest.png"/></a>
+                        <a className='btn2' href="/writtentestclock/clock-in-intro"><img
+                            src="/static/writtentestclock/start_writtentest.png"/></a>
                     </div>
                 </div>
                 <style jsx>{`
                     .written-test-clock {
                         padding: 0!important;
                     }
+                    .test-result-page {
+                        min-height: 100vh;
+                        display: flex;
+                        align-items: center;
+                    }
                     .test-result {
-                        padding: 50px 0;
                         position: relative;
                     }
                     .result-bg {
