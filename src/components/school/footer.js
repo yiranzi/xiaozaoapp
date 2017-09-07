433122f7836a84086250b0418fd87cde3cbb12d7
Xu Lei <alivelee6@gmail.com>
2017-09-07 13:02:19 +0800

    update:change link

--- src/components/school/footer.js
+++ src/components/school/footer.js
@@ -2,39 +2,33 @@ import React from 'react';
 import classNames from 'classnames';
 
 export default class Footer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showDialog: false,
-            style: {
-                buttons: [
-                    {
-                        label: '知道了',
-                        onClick: this.hideDialog.bind(this)
-                    }
-                ]
-            }
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDialog: false,
+    };
+  }
 
-    hideDialog() {
-        this.setState({
-            showDialog: false
-        });
-    }
+  hideDialog() {
+    this.setState({
+      showDialog: false
+    });
+  }
 
-    render() {
-        let { fromType } = this.props;
-        return (
-            <div className="footer">
-                <div className="footer-nav">
-                    <a href="/school/kpmg/schoolprocess" className={classNames('a__bg',{'isActive': fromType == 'kpmg'})}>毕马威</a>
-                    <a href="/school/anyong/schoolprocessl" className={classNames('a__bg',{'isActive': fromType == 'anyong'})}>安永</a>
-                    <a href="/school/pwccn/schoolprocessl" className={classNames('a__bg',{'isActive': fromType == 'pwccn'})}>普华永道</a>
-                    <a href="/school/de/schoolprocess" className={classNames('a__bg',{'isActive': fromType == 'de'})}>德勤</a>
-                    <a onClick={() => alert("更多公司的校招信息和求职干货 会在9月初【校招助手移动端】一同上线")} className="a__bg">更多企业</a>
-                </div>
-                <style jsx>{`
+  render() {
+    let {fromType} = this.props;
+    return (
+      <div className="footer">
+        <div className="footer-nav">
+          <a href="/school/kpmg/schoolprocess" className={classNames('a__bg', {'isActive': fromType == 'kpmg'})}>毕马威</a>
+          <a href="/school/anyong/schoolprocessl"
+             className={classNames('a__bg', {'isActive': fromType == 'anyong'})}>安永</a>
+          <a href="/school/pwccn/schoolprocessl"
+             className={classNames('a__bg', {'isActive': fromType == 'pwccn'})}>普华永道</a>
+          <a href="/school/de/schoolprocess" className={classNames('a__bg', {'isActive': fromType == 'de'})}>德勤</a>
+          <a href="/school/navigation" className="a__bg">更多企业</a>
+        </div>
+        <style jsx>{`
                     .footer-nav {
                         background-color:#F6F6F6;
                         position:fixed;
@@ -53,7 +47,7 @@ export default class Footer extends React.Component {
                         background-color:#EEEEEE;
                     }
                 `}</style>
-            </div>
-        );
-    }
+      </div>
+    );
+  }
 }
