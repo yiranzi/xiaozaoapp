6132fc6743dbd608904cc040475d1a78d03f5810
Xu Lei <alivelee6@gmail.com>
2017-09-11 15:38:52 +0800

    update:company zhaoshang

---
+++ pages/school/zhaoshang/schoolExp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../../../src/components/school/schoolLayout';
+import SchoolProcessExpPage from '../../../src/page/un/schoolprocessExp/index';
+
+export default class extends React.Component {
+  render () {
+    return (
+      <Layout fromType='zhaoshang'>
+        <SchoolProcessExpPage />
+      </Layout>
+    );
+  }
+}
