185d4a9aa3e3d9e319971854fd1e9240b040ab8c
Xu Lei <alivelee6@gmail.com>
2017-09-12 19:35:34 +0800

    update:zhada,ali company

---
+++ pages/school/zhada/schoolWork.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../../../src/components/school/schoolLayout';
+import SchoolProcessWorkPage from '../../../src/page/zhada/schoolprocessWork/index';
+
+export default class extends React.Component {
+  render () {
+    return (
+      <Layout fromType='zhada'>
+        <SchoolProcessWorkPage />
+      </Layout>
+    );
+  }
+}
