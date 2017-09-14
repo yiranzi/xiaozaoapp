eb3465deb364df0f49eceb759df627045d4551e6
Xu Lei <alivelee6@gmail.com>
2017-09-14 16:22:54 +0800

    update:add company and refactor the code

---
+++ pages/school/didi/schoolWork.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../../../src/components/school/schoolLayout';
+import SchoolProcessWorkPage from '../../../src/page/didi/schoolprocessWork/index';
+
+export default class extends React.Component {
+  render () {
+    return (
+      <Layout fromType='didi'>
+        <SchoolProcessWorkPage />
+      </Layout>
+    );
+  }
+}
