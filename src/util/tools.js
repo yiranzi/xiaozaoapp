c61cc200e28ce6953d66aba18290900a1aa40e2a
wangyuanli <yuanli.wang@xiaozao.org>
2017-09-14 11:40:51 +0800

    update: 笔试打卡第二期答题

---
+++ src/util/tools.js
@@ -0,0 +1,10 @@
+let ToolsUtil = {};
+
+ToolsUtil.getQueryString = (name) => {
+  var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+  var r = window.location.search.substr(1).match(reg);
+  if (r != null) return unescape(r[2]);
+  return null;
+};
+
+module.exports = ToolsUtil;
