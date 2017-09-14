bb72b96c38745936e06d3c308d46961ad3d2e20a
wangyuanli <yuanli.wang@xiaozao.org>
2017-09-14 11:40:51 +0800

    update: 笔试打卡第二期答题

--- pages/writtentestclocksecond/task.js
+++ pages/writtentestclocksecond/task.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import WrittenTestClock from '../../src/page/writtentestclock/components/layout';
-import WrittenTestClockAction from '../../src/action/writtentestclock/answer';
+import WrittenTestClockSecondAction from '../../src/action/writtentestclocksecond/index';
 import SubjectComponent from '../../src/page/writtentestclock/components/subject';
 import ToolsUtil from '../../src/util/tools';
+import ThemeConfig from '../../config/theme';
 
 export default class extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
       currentObjectIndex: 0,
-      questionList: {}
+      answerListResult: {},
+      questionList: {},
+      initTime: 0,
+      isShowAnalysis: false
     };
   }
 
   componentDidMount = async () => {
     const category = ToolsUtil.getQueryString('category');
+    const day = ToolsUtil.getQueryString('day');
     let questionList;
-    // 首次测评
-    if (category === 'entrance') {
-      questionList = await WrittenTestClockAction.getEvaluation();
-    } else if (category === 'task') {
-      questionList = await WrittenTestClockAction.getToday();
+    if (category === 'entrance') { // 首次测评
+      questionList = await WrittenTestClockSecondAction.getEvaluation();
     } else if (category === 'review') {
       const day = ToolsUtil.getQueryString('day');
       if (day) {
@@ -30,12 +32,17 @@ export default class extends React.Component {
       }
     } else if (category === 'finish') {
       questionList = await WrittenTestClock.getTest();
+    } else {
+      if (day) {
+        questionList = await WrittenTestClock.getByToday(day);
+      } else {
+        questionList = await WrittenTestClockSecondAction.getToday();
+      }
     }
     this.setState({questionList: questionList});
   };
 
-  renderAnswer (questionList) {
-    const {currentObjectIndex} = this.state;
+  renderAnswer (currentObjectIndex, questionList) {
     const {writtenTestTopicDTOList} = questionList;
     const questionItem = writtenTestTopicDTOList[currentObjectIndex];
     const subjectItem = {
@@ -56,12 +63,149 @@ export default class extends React.Component {
     );
   }
 
+  answerCheck (id, value) {
+    // const {answerList} = this.state;
+    let {answerListResult} = this.state;
+    answerListResult[id] = answerListResult[id] || {};
+    answerListResult[id].tag = value;
+    this.setState({
+      answerListResult: answerListResult
+    });
+  }
+
+  renderAnswerAnalysis (currentObjectIndex, questionList) {
+    const {isShowAnalysis} = this.state;
+    const {answerDTOList, writtenTestTopicDTOList} = questionList;
+    const questionItem = writtenTestTopicDTOList[currentObjectIndex];
+    const {id, answer, analysis} = questionItem;
+    const answerList = this.formatAnswerDTOList(answerDTOList);
+
+    const analysisContent = (
+      <div className='analysis' >
+        <div className='wrapper' >
+          <div className='analysis-header' >
+            <div className='answer' >答案：{answer}</div >
+            <div className='line' />
+            <div className='hide-analysis' >查看解析</div >
+          </div >
+          <div className='analysis-content' >{analysis}</div >
+          <style jsx >{`
+              .analysis-header {
+                display: flex;
+                justify-content: space-between;
+              }
+              .hide-analysis {
+                display: inline-block;
+                border: 1px solid ${ThemeConfig.color.writtentestclockmain};
+                font-size: ${ThemeConfig.size.normal};
+                font-weight: bold;
+                border-radius: 1rem;
+                padding: 0.1rem 0.5rem;
+              }
+            `}</style >
+        </div >
+      </div >
+    );
+
+    if (isShowAnalysis || answerList.hasOwnProperty(id)) {
+      return analysisContent;
+    }
+  }
+
+  formatAnswerDTOList (answerDTOList) {
+    let json = {};
+    answerDTOList.map((item, index) => {
+      json[item.id] = item.answer;
+    });
+    return json;
+  }
+
+  renderFinishButton (currentObjectIndex) {
+    return (
+      <div className='finish' >
+        <div onClick={() => {
+          this.prevAnswer(currentObjectIndex);
+        }} >
+          <img src='/static/writtentestclock/prev.png' />
+        </div >
+        <div onClick={() => {
+          this.answerComplete();
+        }} ><img src='/static/writtentestclock/complete.png' /></div >
+      </div >
+    );
+  }
+  answerComplete = async () => {
+    const {initTime} = this.state;
+    const {setId} = this.state.questionList;
+    const answerList = this.formatAnswerList();
+    console.log(answerList);
+    // try {
+    //   const spendTime = new Date() - initTime;
+    //   const data = JSON.stringify({
+    //     setId: setId,
+    //     time: spendTime,
+    //     answerDTOList: answerList
+    //   });
+    //
+    //   await WrittenTestClockSecondAction.complete(data);
+    //   location.href = '/writtentestclock/clock-in-result';
+    // } catch (e) {
+    //   console.log(e);
+    // }
+  };
+  formatAnswerList () {
+    const {questionList, answerListResult} = this.state;
+    let answerList = [];
+
+    questionList.writtenTestTopicDTOList.map((item, index) => {
+      const {id} = item;
+      let seleteAnswer = answerListResult[id] ? answerListResult[id].tag : '';
+      answerList.push({id: item.id, answer: seleteAnswer});
+    });
+    return answerList;
+  }
+
+  renderActionButton (currentObjectIndex, questions) {
+    return (
+      <div className='action' >
+        <div onClick={() => {
+          this.prevAnswer(currentObjectIndex);
+        }} ><img src='/static/writtentestclock/prev.png' /></div >
+        <div onClick={() => {
+          this.nextAnswer(currentObjectIndex, questions);
+        }} ><img src='/static/writtentestclock/next.png' /></div >
+      </div >
+    );
+  }
+
+  prevAnswer (currentObjectIndex) {
+    this.setState({
+      currentObjectIndex: currentObjectIndex - 1,
+      finish: false
+    });
+  }
+
+  nextAnswer (currentObjectIndex, questionList) {
+    let nextObjectIndex = currentObjectIndex + 1;
+    const {writtenTestTopicDTOList} = questionList;
+    if (nextObjectIndex >= writtenTestTopicDTOList.length - 1) {
+      this.setState({
+        currentObjectIndex: nextObjectIndex,
+        finish: true
+      });
+    } else {
+      this.setState({
+        currentObjectIndex: nextObjectIndex,
+      });
+    }
+  }
+
   render () {
-    const {questionList} = this.state;
+    const {currentObjectIndex, questionList, finish} = this.state;
     if (questionList.hasOwnProperty('setId')) {
       return (
         <WrittenTestClock >
-          {this.renderAnswer(questionList)}
+          {this.renderAnswer(currentObjectIndex, questionList)} {this.renderAnswerAnalysis(currentObjectIndex, questionList)} {finish ? this.renderFinishButton(currentObjectIndex) : this.renderActionButton(currentObjectIndex, questionList)}
         </WrittenTestClock >
       );
     } else {
