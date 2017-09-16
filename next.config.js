4462ecc244011816a5f5917f2b2f84f038f2bae3
wangyuanli <yuanli.wang@xiaozao.org>
2017-09-16 10:46:51 +0800

    chore: export path by script

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const glob = require('glob');
+
+const ToolsUtil = require('./src/util/tools');
+
 module.exports = {
   webpack: (config, {dev}) => {
     // 处理weui样式
@@ -36,46 +39,10 @@ module.exports = {
         exclude: /node_modules/
       });
     }
+    config.target = 'async-node';
     return config;
   },
   exportPathMap: function () {
-    return {
-      '/school/anyong/schoolExp': {page: '/school/anyong/schoolExp'},
-      '/school/anyong/schoolprocess': {page: '/school/anyong/schoolprocess'},
-      '/school/anyong/schoolWork': {page: '/school/anyong/schoolWork'},
-      '/school/de/schoolExp': {page: '/school/de/schoolExp'},
-      '/school/de/schoolprocess': {page: '/school/de/schoolprocess'},
-      '/school/de/schoolWork': {page: '/school/de/schoolWork'},
-      '/school/kpmg/schoolExp': {page: '/school/kpmg/schoolExp'},
-      '/school/kpmg/schoolprocess': {page: '/school/kpmg/schoolprocess'},
-      '/school/kpmg/schoolWork': {page: '/school/kpmg/schoolWork'},
-      '/school/pwccn/schoolExp': {page: '/school/pwccn/schoolExp'},
-      '/school/pwccn/schoolprocess': {page: '/school/pwccn/schoolprocess'},
-      '/school/pwccn/schoolWork': {page: '/school/pwccn/schoolWork'},
-      '/school/mars/schoolExp': {page: '/school/mars/schoolExp'},
-      '/school/mars/schoolprocess': {page: '/school/mars/schoolprocess'},
-      '/school/mars/schoolWork': {page: '/school/mars/schoolWork'},
-      '/school/mck/schoolExp': {page: '/school/mck/schoolExp'},
-      '/school/mck/schoolprocess': {page: '/school/mck/schoolprocess'},
-      '/school/mck/schoolWork': {page: '/school/mck/schoolWork'},
-      '/school/un/schoolExp': {page: '/school/un/schoolExp'},
-      '/school/un/schoolprocess': {page: '/school/un/schoolprocess'},
-      '/school/un/schoolWork': {page: '/school/un/schoolWork'},
-      '/school/mercer/schoolExp': {page: '/school/mercer/schoolExp'},
-      '/school/mercer/schoolprocess': {page: '/school/mercer/schoolprocess'},
-      '/school/mercer/schoolWork': {page: '/school/mercer/schoolWork'},
-      '/writtentestclock/test': {page: '/writtentestclock/test'},
-      '/writtentestclock/answer': {page: '/writtentestclock/answer'},
-      '/writtentestclock/pastanswer': {page: '/writtentestclock/pastanswer'},
-      '/writtentestclock/choose-class': {page: '/writtentestclock/choose-class'},
-      '/writtentestclock/clock-in-intro': {page: '/writtentestclock/clock-in-intro'},
-      '/writtentestclock/clock-in-result': {page: '/writtentestclock/clock-in-result'},
-      '/writtentestclock/daily-clock-in': {page: '/writtentestclock/daily-clock-in'},
-      '/writtentestclock/former-clock-in': {page: '/writtentestclock/former-clock-in'},
-      '/writtentestclock/index-clock-in': {page: '/writtentestclock/index-clock-in'},
-      '/writtentestclock/test-entry': {page: '/writtentestclock/test-entry'},
-      '/writtentestclock/test-result': {page: '/writtentestclock/test-result'},
-      '/writtentestclock/more-test': {page: '/writtentestclock/more-test'}
-    };
+    return ToolsUtil.exportPathMap();
   }
 };
