c49204ef8314c8ccd510a3bcea0ea70d4b9c3f6f
leonzhang <human1108@163.com>
2017-09-18 11:39:34 +0800

    update: 打卡第二期 clock-in-intro

---
+++ config/theme-season-2.js
@@ -0,0 +1,14 @@
+const config = {
+    color: {
+      white: '#fff',
+      writtentestclockmain: '#ec4242'
+    },
+    size: {
+      small: '1rem',
+      normal: '1.25rem',
+      large: '1.5rem'
+    }
+  }
+  
+  module.exports = config
+  
