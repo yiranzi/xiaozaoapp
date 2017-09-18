c92bdf89e8d8bcf3634601b823152eede55d8a09
leonzhang <human1108@163.com>
2017-09-18 15:54:21 +0800

    update: 打卡第二期 test-entry

--- pages/writtentestclocksecond/test-entry.js
+++ pages/writtentestclocksecond/test-entry.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import WrittenTestClock from '../../containers/writtentestclock/components/layout'
+import WrittenTestClock from '../../containers/writtentestclocksecond/layout'
 import { Flex, FlexItem } from 'react-weui'
 
 export default class extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      content: ['恭喜你！', '离四大又近了一步', '不过，你也一定很好奇：',
-        '以我现在的笔试水平，到底有多大把握进四大？', '为了避免', '你在高级班跟不上节奏的尴尬',
-        '在初级班又有孤独求败的体验', '不如趁现在！', '我们花几分钟来个小测试', '搞清楚自己是报笔试基础班还是高级班吧！']
+      content: ['你一定也很好奇', '以我现在的笔试水平', '到底有多大把握进四大？',
+        '为了避免', '你在高级班跟不上节奏的尴尬',
+        '在初级班又有孤独求败的体验', '不如趁现在！', '我们花几分钟来个小测试', '选择最适合自己的班级吧！']
     }
   }
 
@@ -16,17 +16,45 @@ export default class extends React.Component {
     return (
       <style jsx global>{`
           .tips-form {
-              flex: 1;
-              border: 1px solid;
-              margin: 10px;
-              position: relative;
-              padding: 30px 0;
+            display: flex;
+            flex-direction: column;
+            position: relative;
+            background: url(/static/writtentestclocksecond/entry-form.png);
+            background-size: 100%;
+            background-repeat: no-repeat;
+            margin: 0 10vw;
+            padding: 4vh 0;
+            justify-content: space-between;
+            height: 44vh;
+          }
+          .tips-form:before {
+            content: ' ';
+            position: absolute;
+            background: url(/static/writtentestclocksecond/entry-form-left.png);
+            left: -34px;
+            width: 100px;
+            top: 9%;
+            height: 111px;
+            background-size: 35%;
+            background-repeat: no-repeat;
+          }
+          .tips-form:after {
+            content: ' ';
+            position: absolute;
+            background: url(/static/writtentestclocksecond/entry-form-right.png);
+            right: -99px;
+            width: 100px;
+            top: -25%;
+            height: 200px;
+            background-size: 45%;
+            background-repeat: no-repeat;
           }
           .tips {
-              text-align: center;
-              margin: 10px 0;
-              text-shadow: 0px 7px 3px #000;
-              font-size: 15px;
+            text-align: center;
+            margin: 5px 0;
+            color: #fff;
+            font-size: 14px;
+            font-weight: 200;
           }
       `}</style>
     )
@@ -43,84 +71,61 @@ export default class extends React.Component {
       <WrittenTestClock>
         <Flex >
           <FlexItem>
-            <div className='title'>小灶四大笔试七天打卡计划</div>
+            <div className='title'>恭喜你！</div>
+            <div className='sub-title'>离 四 大 又 近 了 一 步</div>
           </FlexItem>
         </Flex>
-        <Flex>
-          <FlexItem className='tips-form'>
-            <div className='corner-top-left corner' />
-            <div className='corner-top-right corner' />
-            <div className='corner-bottom-left corner' />
-            <div className='corner-bottom-right corner' />
-            {this.renderContent()}
-          </FlexItem>
-        </Flex>
-        <Flex>
-          <FlexItem>
-            <a className='img' href='/writtentestclocksecond/task?category=entrance'>马上点击测试</a>
-          </FlexItem>
-          <FlexItem>
-            <a className='img' href='/writtentestclocksecond/clock-in-intro'>直接选择班级</a>
-          </FlexItem>
-        </Flex>
+        <div className='tips-form'>
+          {this.renderContent()}
+        </div>
+        <div className='bottom-btn'>
+              <a className='btn-test' href='/writtentestclocksecond/task?category=entrance'></a>
+              <a className='btn-choose' href='/writtentestclocksecond/clock-in-intro'></a>
+            </div>
+        
         {this.renderGlobalCss()}
         <style jsx>{`
-            .title {
-                font-size: 25px;
-                font-weight: bold;
-                text-align: center;
-                margin-top: 20px;
-            }
-            
-            .corner {
-                height: 20px;
-                width: 20px;
-                position: absolute;
-                border: 1px solid;
-                background: rgb(30,31,32);
-            }
-            .corner-top-left {
-                top: -1px;
-                left: -1px;
-                border-radius: 0 0 20px 0;
-                border-top: none;
-                border-left: none;
-            }
-            .corner-top-right {
-                top: -1px;
-                right: -1px;
-                border-radius: 0 0 0 20px;
-                border-top: none;
-                border-right: none;
-            }
-            .corner-bottom-left {
-                bottom: -1px;
-                left: -1px;
-                border-radius: 0 20px 0 0;
-                border-bottom: none;
-                border-left: none;
-            }
-            .corner-bottom-right {
-                bottom: -1px;
-                right: -1px;
-                border-radius: 20px 0 0 0;
-                border-bottom: none;
-                border-right: none;
-            }
-            .img {
-                color: #000;
-                display: block;
-                background-image: url(/static/writtentestclock/round-btn.png);
-                background-repeat: no-repeat;
-                background-size: 100% 100%;
-                text-align: center;
-                height: 50px;
-                width: 150px;
-                margin: 0 auto;
-                padding-right: 10px;
-                line-height: 36px;
-                margin-top: 20px;
-            }
+          .title {
+            font-size: 35px;
+            font-weight: bold;
+            text-align: center;
+            margin-top: 20px;
+          }
+          .sub-title {
+            font-size: 25px;
+            font-weight: bold;
+            text-align: center;
+          }
+          .corner {
+            height: 20px;
+            width: 20px;
+            position: absolute;
+            border: 1px solid;
+            background: rgb(30,31,32);
+          }
+          .btn-test,
+          .btn-choose {
+            display: block;
+            background-repeat: no-repeat;
+            background-size: 100%;
+            text-align: center;
+            height: 110px;
+            width: 45vw;
+            margin: 0 auto;
+            padding-right: 10px;
+            line-height: 36px;
+            margin-top: 65px;
+          }
+          .btn-test {
+            background-image: url(/static/writtentestclocksecond/entry-btn-left.png);
+          }
+          .btn-choose {
+            background-image: url(/static/writtentestclocksecond/entry-btn-right.png);
+          }
+          .bottom-btn {
+            display: flex;
+            margin-top: -93px;
+          }
         `}</style>
       </WrittenTestClock>
     )
