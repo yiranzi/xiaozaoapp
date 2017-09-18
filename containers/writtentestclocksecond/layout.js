aff57b6317d1cc0728b17a0898acb9f9612e34c7
wangyuanli <yuanli.wang@xiaozao.org>
2017-09-18 11:56:58 +0800

    update: 笔试打卡第二期layout

--- containers/writtentestclocksecond/layout.js
+++ containers/writtentestclocksecond/layout.js
@@ -1,29 +1,24 @@
 import React from 'react'
 import Layout from '../../components/layout'
-import ThemeConfig from '../../../../config/theme'
 
-export default class WrittenTestClock extends React.Component {
+export default class WrittenTestClockSecond extends React.Component {
   renderGlobalCss () {
     return (
       <style global jsx>{`
-                .written-test-clock {
-                    background: url('/static/writtentestclock/bg.jpg');
-                    min-height: 100vh;
-                    color: ${ThemeConfig.color.writtentestclockmain};
-                    padding: 0 1rem;
-                    background-size: 100%;
-                }
-                .written-test-clock-answer {
-                    padding: 1rem 0;
-                }
-            `}</style>
+        .written-test-clock-second {
+          background: url('/static/writtentestclocksecond/bg.png');
+          min-height: 100vh;
+          padding: 0 1rem;
+          background-size: 100% 100%;
+        }
+    `}</style>
     )
   }
   render () {
     const {error} = this.props
     return (
       <Layout error={error}>
-        <div className='written-test-clock'>
+        <div className='written-test-clock-second'>
           {this.props.children}
         </div>
         {this.renderGlobalCss()}
