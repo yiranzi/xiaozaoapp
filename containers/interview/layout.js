6c875819c7fe046deec0076ed0bdf401653d6cb7
wangyuanli <yuanli.wang@xiaozao.org>
2017-10-11 17:12:19 +0800

    update: button style

--- containers/interview/layout.js
+++ containers/interview/layout.js
@@ -38,6 +38,11 @@ export default class extends React.Component {
             color: ${ThemeConfig.color.white};
             background-color: ${ThemeConfig.color.yellow};
           }
+          .interview .disabled .weui-btn_primary,
+          .interview .disabled .weui-btn_primary:not(.weui-btn_disabled):active{
+            background: ${ThemeConfig.color.gray};
+            color: rgba(0,0,0,0.2);
+          }
         `}</style>
       </Layout>
     )
