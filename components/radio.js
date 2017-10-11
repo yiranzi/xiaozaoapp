c31b839665421a83c851b1339115624d31357445
wangyuanli <yuanli.wang@xiaozao.org>
2017-10-11 17:11:35 +0800

    update: radio component

--- components/radio.js
+++ components/radio.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import {FormCell, CellBody, CellFooter, Radio} from 'react-weui'
 
 export default class Layout extends React.Component {
   render () {
@@ -9,26 +8,82 @@ export default class Layout extends React.Component {
     let checked = defaultValue === value
 
     return (
-      <FormCell radio>
-        <CellFooter>
-          {disabled &&
-            <Radio name={name}
-              value={value}
-              defaultChecked={checked}
-              disabled
-            />
+      <div>
+        {disabled &&
+        <label>
+          <input
+            className='radio'
+            type='radio'
+            name={name}
+            value={value}
+            checked={checked}
+            disabled
+          />
+          {label}
+        </label>
+        }
+        {!disabled &&
+        <label>
+          <input
+            className='radio'
+            type='radio'
+            name={name}
+            value={value}
+            onClick={() => onChange(value)}
+          />
+          <div className='text'>{label}</div>
+        </label>
+        }
+        <style jsx>{`
+          label {
+            height: 1.25rem;
           }
-          {!disabled &&
-            <Radio
-              name={name}
-              value={value}
-              defaultChecked={checked}
-              onClick={() => { onChange(value) }}
-            />
+          .text {
+            display: inline-block;
+            vertical-align: middle;
           }
-        </CellFooter>
-        <CellBody>{label}</CellBody>
-      </FormCell>
+          .radio {
+            width: 1rem;
+            height: 1rem;
+            display: inline-block;
+            position: relative;
+            margin-right: 1rem;
+            vertical-align: middle;
+          }
+          .radio::before {
+            content: '';
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            left: 0;
+            top: 0;
+            background-color: #F9F9F9;
+            border: 1px solid #2b3a64;
+            border-radius: 2rem;
+            box-sizing: border-box;
+          }
+          input[type="radio"]:checked::before {
+            content: '';
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            left: 0;
+            top: 0;
+            background-color: #F9F9F9;
+            border-radius: 2rem;
+          }
+          input[type="radio"]:checked::after {
+            content: '';
+            position: absolute;
+            width: 1.5rem;
+            height: 1.25rem;
+            left: -0.15rem;
+            top: -0.25rem;
+            background: url(/static/img/interview/check.png) no-repeat;
+            background-size: 100%;
+          }
+        `}</style>
+      </div>
     )
   }
 }
