e6a10c101e5055630aebe2eb4f8d1b561e52721c
wangyuanli <yuanli.wang@xiaozao.org>
2017-10-21 18:43:18 +0800

    update img format

--- util/data.js
+++ util/data.js
@@ -13,4 +13,28 @@ dataUtil.isEmpty = function (data) {
   return data.length === 0
 }
 
+dataUtil.imgFormat = function (base64, type) {
+  let text = window.atob(base64.split(',')[1])
+  let buffer = new Uint8Array(text.length)
+  for (let i = 0; i < text.length; i++) {
+    buffer[i] = text.charCodeAt(i)
+  }
+  let blob = getBlob([buffer], type)
+  let formdata = new FormData()
+  formdata.append('file', blob, `blob.${type.split('/')[1]}`)
+  return formdata
+
+  function getBlob (buffer, format) {
+    try {
+      return new Blob(buffer, {type: format})
+    } catch (e) {
+      let bb = new (window.BlobBuilder || window.WebKitBlobBuilder || window.MSBlobBuilder)()
+      buffer.forEach(function (buf) {
+        bb.append(buf)
+      })
+      return bb.getBlob(format)
+    }
+  }
+}
+
 module.exports = dataUtil
