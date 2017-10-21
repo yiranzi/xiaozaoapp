40c0896f1892e36d43110cd3e9781efbad694526
wangyuanli <yuanli.wang@xiaozao.org>
2017-10-21 18:33:15 +0800

    update textarea 增加onChange属性

--- components/textarea.js
+++ components/textarea.js
@@ -3,12 +3,12 @@ import {Form, FormCell, CellBody, TextArea} from 'react-weui'
 
 export default class Layout extends React.Component {
   render () {
-    const {placeholder, maxLength} = this.props
+    const {placeholder, maxLength, onChange} = this.props
     return (
       <Form className='textarea'>
         <FormCell>
           <CellBody>
-            <TextArea placeholder={placeholder} maxLength={maxLength} onChange={(e) => this.props.onChange(e)} />
+            <TextArea placeholder={placeholder} maxLength={maxLength} onChange={(e) => onChange(e.target.value)} />
           </CellBody>
         </FormCell>
       </Form>
