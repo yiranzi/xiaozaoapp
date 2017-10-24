e37f3d4fa7df7a45ce53bf5ab6b1e1d65ebdea54
yiranzi <570932296@qq.com>
2017-10-24 17:30:59 +0800

    修改支付

--- util/wxPayController.js
+++ util/wxPayController.js
@@ -31,20 +31,20 @@ wxPayController.pay = () => {
 wxPayController.onBridgeReady = () => {
 // 2 设置订单
   console.log('onBridgeReady')
-  let {appId,  , paySign, timeStamp, prepayId} = payData
+  let {appId, nonceStr, paySign, timeStamp, prepayId} = payData
   WeixinJSBridge.invoke(
     'getBrandWCPayRequest', {
-      "appId": appId,     //公众号名称，由商户传入
-      "timeStamp": timeStamp,         //时间戳，自1970年以来的秒数
-      "nonceStr": nonceStr, //随机串
-      "package":`prepay_id=${prepayId}`,
-      "signType":"MD5",         //微信签名方式：
-      "paySign": paySign //微信签名
+      'appId': appId,
+      'timeStamp': timeStamp,
+      'nonceStr': nonceStr,
+      'package': `prepay_id=${prepayId}`,
+      'signType': 'MD5',
+      'paySign': paySign
     },
     function(res){
-      if(res.err_msg == "get_brand_wcpay_request:ok" ) {
+      if (res.err_msg == 'get_brand_wcpay_request:ok' ) {
         console.log('get ')
-      }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+      }
       location.href = `/interviewvip/introPage`
     }
   );
