71d2b9625130b990f297c1f42095d3d3c20c467d
wangyuanli <yuanli.wang@xiaozao.org>
2017-10-25 20:04:56 +0800

    update: 登录过期跳转

--- util/axios.js
+++ util/axios.js
@@ -20,7 +20,7 @@ function request (param) {
     }).catch((error) => {
       if (error.response.status === 401) {
         alert('登录已过期，请重新登录')
-        // location.href = 'http://wx.xiaozao.org/auth/logout'
+        location.href = 'http://wx.xiaozao.org/auth/logout'
       } else {
         reject(error.message)
       }
