a2da0c5b61d8af0c3d7895c354dae8d46b82e21a
wangyuanli <yuanli.wang@xiaozao.org>
2017-10-30 15:25:53 +0800

    update: back样式

--- components/back.js
+++ components/back.js
@@ -5,14 +5,30 @@ export default class MyBack extends React.Component {
     const {direct, text} = this.props
     return (
       <div>
-        {direct === 'left' && <span className='left'>◂{text}</span>}
-        {direct === 'right' && <span className='right'>{text}▸</span>}
+        {direct === 'left' && <span className='left'>{text}</span>}
+        {direct === 'right' && <span className='right'>{text}</span>}
         <style jsx>{`
           span {
             border: 1px solid #117ee9;
-            padding: 0.1rem 0.5rem;
+            padding: 1px 5px 1px 12px;
             border-radius: 1rem;
             display: inline-block;
+            position: relative;
+          }
+          span.left::before {
+            content: '◂';
+            position: absolute;
+            left: 4px;
+            top: 0;
+          }
+          span.right {
+            padding: 1px 12px 1px 5px;
+          }
+          span.right::before {
+            content: '▸';
+            position: absolute;
+            right: 4px;
+            top: 0;
           }
         `}</style>
       </div>
