c5e9e5291a07dc0a5829f20cda265221898f6ae6
wangyuanli <yuanli.wang@xiaozao.org>
2017-11-01 17:25:52 +0800

    update: 做题解析页面

---
+++ pages/interviewvip/experience/review.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import InterviewLayout from '../../../containers/interviewvip/layout'
+
+export default class extends React.Component {
+  render () {
+    return (
+      <InterviewLayout>解析页面</InterviewLayout>
+    )
+  }
+}
---
+++ pages/interviewvip/experience/task.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import InterviewLayout from '../../../containers/interviewvip/layout'
+
+export default class extends React.Component {
+  render () {
+    return (
+      <InterviewLayout>做题页面</InterviewLayout>
+    )
+  }
+}
