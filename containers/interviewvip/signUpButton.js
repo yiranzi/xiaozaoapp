f47f5de7b46f5f7b3b4bfd674a27a8f32e54e4fc
yiranzi <570932296@qq.com>
2017-11-01 16:50:32 +0800

    update: 课程购买页 按钮栏

--- containers/interviewvip/signUpButton.js
+++ containers/interviewvip/signUpButton.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import {Button} from 'react-weui'// 组件库
+import Button from '../../components/button'
 import ThemeConfig from '../../config/theme'
 import classNames from 'classnames'
+import Fixfooter from '../../components/fixfooter'
 
 /*
 props
@@ -17,18 +18,38 @@ props
  */
 
 export default class extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      seeButton: false
+    }
+  }
   render () {
     return (
-      <div className='button'>
-        {this.renderButtonBar()}
+      <div >
+        <div className='fixfooter-container'>
+          {<div className='button'>{this.renderButtonBar()}</div>}
+        </div>
+        {this.renderButtonList()}
         <style jsx>{`
-          .button {
-            display: flex;
-            justify-content: space-between;
+          .fixfooter-container {
+            background-color: #F9F9F9;
+            border-top: 1px solid #e5e5e5;
             position: fixed;
             width: 100%;
-            left: 0;
+            {/*padding: 10px;*/}
+            height: 60px;
+            line-height: 60px;
+            box-sizing: border-box;
+            z-index: 100;
             bottom: 0;
+            left: 0;
+          }
+          .button {
+            height: 100%
+            display: flex;
+            justify-content: space-between;
+            width: 100%;
             padding: 5px;
             box-sizing: border-box;
             background: #F9F9F9;
@@ -68,15 +89,6 @@ export default class extends React.Component {
           .weui-btn_primary.pay {
             width: 50%;
           }
-          .price-button1 {
-            background-color: #fe0000 !important;
-          }
-          .price-button2 {
-            background-color: #ff5d5d !important;
-          }
-          .price-button3 {
-            background-color: ${ThemeConfig.color.blue} !important;
-          }
         `}</style>
       </div>
     )
@@ -84,61 +96,130 @@ export default class extends React.Component {
 
   renderButtonBar () {
     let arr = []
+    let style = {
+      width: '100%',
+      margin: 'auto 5px auto 5px'
+    }
     // 1是否购买
     if (this.props.payStatus) {
       // 2是否开课, 根据不同的状态判断
-      arr.push(this.renderFreeTry(true))
-      arr.push(this.renderEnter(this.props.canEnter))
+      arr.push(<div key={1} style={style}>{this.renderAskInfo(true)}</div>)
+      arr.push(<div key={6} style={style}>{this.renderEnter(this.props.canEnter)}</div>)
     } else {
-      arr.push(this.renderFreeTry())
+      arr.push(<div key={2} style={style}>{this.renderAskInfo()}</div>)
+      arr.push(<div key={3} style={style}>{this.renderFreeTry()}</div>)
       if (this.props.canBuy) {
-        arr.push(this.renderCut3())
-        arr.push(this.renderCut2())
-        arr.push(this.renderCut1())
+        arr.push(<div key={4} style={style}>{this.renderBuyButtonGroup()}</div>)
       } else {
-        arr.push(this.renderHaveClosed())
+        arr.push(<div key={5} style={style}>{this.renderHaveClosed()}</div>)
       }
     }
     return arr
   }
+
+  renderButtonList () {
+    let style = {
+      margin: '5px'
+    }
+    let style2 = {}
+    if (this.state.seeButton) {
+      style2 = {
+        bottom: '60px'
+      }
+    } else {
+      style2 = {
+        bottom: '-100px'
+      }
+    }
+    return <div style={style2}className='fix-button-list'>
+      <div style={style}>{this.renderCut1()}</div>
+      <div style={style}>{this.renderCut2()}</div>
+      <div style={style}>{this.renderCut3()}</div>
+      <style jsx>{
+        ` .fix-button-list {
+            position: fixed;
+            right: 0;
+            width: 33%;
+            background-color: white;
+            transition: bottom 0.5s;
+            overflow: hidden;
+          }`
+      }</style>
+    </div>
+  }
+
+  //arr列表
 
   /*
   没有开课的进入打卡按钮
    */
   renderEnter (state) {
     if (state) {
-      return <Button className='enter' onClick={() => { this.goPath('/interviewvip/list') }}>进入群面</Button>
+      return <Button bg={`${ThemeConfig.color.yellow}`} text={'进入群面'} onClick={() => { this.goPath('/interviewvip/list') }} />
     } else {
-      return <Button type='default' disabled>进入群面</Button>
+      return <Button text={'未开始'} type='default' disabled />
     }
   }
 
-  renderFreeTry (state) {
+  /*
+   没有开课的进入打卡按钮
+   */
+
+  renderFreeTry () {
+    return (
+      <Button className='price-button1' onClick={() => { this.goPath('/interviewvip/experience/intro') }} text={'抢先体验'} />
+    )
+  }
+
+  renderAskInfo (state) {
     return (
       <Button
         className={classNames('experience', {'pay': state})}
+        text={'立即咨询'}
         onClick={() => { this.goPath('https://shimo.im/doc/hMWImuwPj4Q1ZEYj?r=NZOD95') }}
-      >{this.props.buttonContent}</Button>
+      />
     )
   }
-  renderCut1 () {
+
+  clickBuyGroupButton () {
+    console.log('clickBuyGroupButton')
+    let bool = !this.state.seeButton
+    this.setState({
+      seeButton: bool
+    })
+  }
+
+  renderBuyButtonGroup () {
+    let content
+    if (this.state.seeButton) {
+      content = '优惠报名↓'
+    } else {
+      content = '优惠报名↑'
+    }
     return (
-      <Button className='price-button1' onClick={() => { this.goPath('https://kdt.im/Rqyg0h') }}>299拼团<img src='https://pic.sopili.net/pub/emoji/twitter/2/72x72/1f525.png' style={{width: '20px', height: '20px'}} /></Button>
+      <Button bg={'rgb(255, 93, 93)'} onClick={this.clickBuyGroupButton.bind(this)} text={content} />
+    )
+  }
+  renderCut3 () {
+    let content = <div>3人团299<img src='https://pic.sopili.net/pub/emoji/twitter/2/72x72/1f525.png' style={{width: '20px', height: '20px'}} /></div>
+    // content = <div>299拼团</div>
+    return (
+      <Button bg={'#fe0000'} onClick={() => { this.goPath('https://kdt.im/Rqyg0h') }} text={content} />
     )
   }
   renderCut2 () {
     return (
-      <Button className='price-button2' onClick={() => { this.goPath('https://kdt.im/p8yg0h') }}>349拼团</Button>
+      <Button bg={'#ff5d5d'} onClick={() => { this.goPath('https://kdt.im/p8yg0h') }} text='2人团349' />
     )
   }
-  renderCut3 () {
+  renderCut1 () {
     return (
-      <Button className='price-button3' onClick={() => { this.goPath('https://kdt.im/r1Ig0h') }}>399原价</Button>
+      <Button bg={`${ThemeConfig.color.blue}`} onClick={() => { this.goPath('https://kdt.im/r1Ig0h') }} text='单人399' />
     )
   }
 
   renderHaveClosed () {
-    return <Button>报名已截止</Button>
+    return <Button title='报名已截止' />
   }
 
   // 组件回调父组件
---
+++ pages/interviewvip/resultFree.js
@@ -0,0 +1,183 @@
+import React from 'react'// 库
+import Button from '../../components/button'// 组件库
+import Back from '../../components/back'
+import Fixfooter from '../../components/fixfooter'
+import resultContent from '../../containers/interviewvip/result/resultContent'
+import ReadMore from '../../containers/interviewvip/result/ReadMore'
+import InterviewLayout from '../../containers/interviewvip/layout'// container
+import ToolsUtil from '../../util/tools'
+import ThemeConfig from '../../config/theme'
+
+import CourseInfo from '../../util/getCourseInfo'
+
+export default class extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      topicKey: null,
+      isRender: true,
+      error: '',
+      interviewResult: null,
+      todayDoing: false,
+      isShowNextButton: false,
+      nextTaskUrl: '',
+      buttonWord: ''
+    }
+  }
+
+  componentDidMount = async () => {
+    let topicKey = ToolsUtil.getQueryString('topicKey')
+
+    let payStatus = CourseInfo.getPayStatus()
+    // 体验 or 购买
+    if (payStatus) {
+      try {
+        let list = await CourseInfo.getList()
+        this.setState({
+          list: list
+        })
+      } catch (e) {
+        // 未付费 渲染报错信息.不渲染列表
+        this.setState({
+          error: e.message
+        })
+      }
+      let result = CourseInfo.isLast(topicKey)
+      let {taskUrl, show, word} = result
+      this.setState({
+        topicKey: topicKey,
+        isShowNextButton: show,
+        buttonWord: word,
+        nextTaskUrl: taskUrl,
+        isRender: false,
+        payStatus: payStatus
+      })
+    } else {
+      this.setState({
+        topicKey: 'free',
+        payStatus: payStatus,
+        isRender: false
+      })
+    }
+  }
+
+  /*
+   根据下一课的状态 显示按钮
+   下一课 没做 就有按钮.
+   */
+  renderButtonState () {
+    if (!this.state.payStatus) {
+      let payButton = <Button onClick={() => { location.href = '/interviewvip/introPage' }} key={1} half text={'立即购买'} />
+      return (<Fixfooter content={payButton} />)
+    } else {
+      let ele = <Button onClick={this.goRouter.bind(this, this.state.nextTaskUrl)} key={1}half text={this.state.buttonWord} />
+      if (this.state.isShowNextButton) {
+        return (<Fixfooter content={ele} />)
+      } else {
+        return null
+      }
+    }
+  }
+
+  goRouter (topicKey) {
+    // 进入下一节的介绍页
+    location.href = `/interviewvip/intro?topicKey=${topicKey}`
+  }
+
+  render () {
+    const {isRender, error} = this.state
+    return (
+      <InterviewLayout isRender={isRender} error={error}>
+        <div className='result-page'>
+          <a href='/interviewvip/list'><Back direct='left' text='返回主页' /></a>
+          {this.renderResultContent()}
+          <ReadMore topicKey={this.state.topicKey} />
+          {this.renderButtonState()}
+        </div>
+        <style jsx>{`
+          a {
+            color: ${ThemeConfig.color.blue};
+          }
+          .result-page {
+            padding-bottom: 200px;
+          }
+        `}</style>
+      </InterviewLayout>
+    )
+  }
+
+  seeReview () {
+    if (this.state.payStatus) {
+      location.href = `/interviewvip/review?topicKey=${this.state.topicKey}`
+    } else {
+      console.log(123)
+      location.href = `/interviewvip/payment`
+    }
+  }
+
+  renderResultContent () {
+    let style = {
+      width: '70%',
+      margin: '0 auto 20px auto'
+    }
+    let content = resultContent[this.state.topicKey]
+    return (
+      <div className='main'>
+        <div className='out'>
+          <img className='bg'src='/static/img/interviewvip/result/resultBg-bg.png' />
+          <div className='inner'>
+            <h1 className='title'>恭喜你完成本章学习</h1>
+            <div dangerouslySetInnerHTML={{__html: content}} />
+          </div>
+        </div>
+        <div style={style}>
+          <Button onClick={() => { this.seeReview.bind(this) }}
+            bg={ThemeConfig.color.yellow}
+            color={'white'}
+            text={'查看答案及解析'} />
+        </div>
+        <style jsx>
+          {`
+          .out {
+            position: relative;
+          }
+          .title {
+            font-size: 1.1rem;
+            margin: 20px 0 10px 0;
+          }
+          .main {
+            margin: 10px auto 20px auto;
+            border-top: 1px solid #e5e5e5;
+            border-bottom: 1px solid #e5e5e5;
+            font-size: 0;
+            text-align: center;
+          }
+          .bg {
+            position: relative;
+            margin: 15px auto 20px auto;
+            width: 100%;
+            height: 175px;
+          }
+          .inner {
+            {/*margin: 10px auto 0 auto;*/}
+            text-align: center;
+            display: flex;
+            justify-content: center;
+            flex-wrap: wrap;
+            position: absolute;
+            top: 0;
+            left: 15px;
+            width: 90%;
+            color: white;
+            font-size: 0.9rem;
+            text-shadow: 1px 1px 1px rgba(0,0,0,0.2);
+          }
+          .inner p {
+            width: 100%;
+          }
+          `}
+        </style>
+      </div>
+    )
+  }
+}
