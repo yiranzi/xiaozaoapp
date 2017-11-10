7c76aa614f865c8279e1afbd21d0f91d1b884c1e
yiranzi <570932296@qq.com>
2017-11-10 20:50:54 +0800

    update:  修改分享链接

---
+++ pages/learncard/redirect.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class extends React.Component {
+  componentDidMount = () => {
+    location.href = 'https://mp.weixin.qq.com/s/wove3tSQ5cEG7cXvQWo_FA'
+  }
+
+  render () {
+    return null
+  }
+}
