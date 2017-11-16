bea9d3a877e8785770b1e4c5d51e4bbe775e2f7e
yiranzi <570932296@qq.com>
2017-11-16 14:39:24 +0800

    update:  修改补充上tabbar

--- xz-components/tabbar.js
+++ xz-components/tabbar.js
@@ -1,67 +1,95 @@
 
 import React from 'react'
 
-// divArr 数组
-// currentSelect //当前选中的按钮
-// buttonStatus //描述每个按钮的状态
-// disabledStyle  //不可选中状态按钮
-// chooseStyle  // 选中状态按钮
-// normalStyle // 正常状态按钮
 
-export default class extends React.Component {
+/**
+ * param
+ * disabledStyle  //不可选中状态按钮
+ * chooseStyle  // 选中状态按钮
+ * normalStyle // 正常状态按钮
+ * onChange // 变动回调
+ * onTabClick // 点击tabbar回调
+ *
+ * by yiran
+ */
+
+export class Tabbar extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      currentSelect: 0
+    }
+    this.onChange = this.onChange.bind(this)
+    this.onTabClick = this.onTabClick.bind(this)
+    this.renderMe = this.renderMe.bind(this)
+  }
+
+  componentDidMount () {
+    this.setState({
+      currentSelect: this.props.defaultActiveKey
+    })
+  }
+
   render () {
     return (
-      <div className='tabbar'>
+      <div style={this.props.tabStyle} onClick={()=>{console.log('tag!!!!!')}}>
         {this.renderMe()}
-        {/* tabbar样式 */}
-        <style jsx>{`
-          .tabbar {
-            width: 250px;
-            margin: auto;
-          }
-        `}</style>
       </div>
     )
   }
 
-  // 渲染容器
-  // 填充容器
-  // 判定状态
-  // 指定回调
   renderMe () {
+    let {tabs} = this.props
     let style
-    let clickBoxArr = this.props.divArr.map((ele, index) => {
-      style = this.calcStyle(index)
-      // eslint-disable-next-line
-      return <div className='contain' key={index} style={style} onClick={this.onClickTabbar.bind(this, index)}>
-        {this.props.divArr[index]}
-        {/* 内部元素 */}
-        <style jsx>{`
-          .contain {
-            margin: 20px auto 20px auto;
-          }
-        `}</style>
+    let tabsArray = tabs.map((ele, index) => {
+      style = this.calcStyle(ele, index)
+      return <div key={index} style={style} onClick={() => { this.onTabClick(ele, index) }}>
+        <p>{ele.title}</p>
       </div>
     })
-    return clickBoxArr
+    return tabsArray
   }
 
-  calcStyle (index) {
+  calcStyle (ele, index) {
     // 1 是否是选中
     // eslint-disable-next-line
-    let {currentSelect, buttonStatus, normalStyle, chooseStyle, disabledStyle} = this.props
-    if (index === currentSelect) {
-      return chooseStyle
+    let {normalStyle, chooseStyle, disabledStyle} = this.props
+    if (index === this.state.currentSelect) {
+      return this.myNew(normalStyle, chooseStyle)
     } else {
-      if (buttonStatus[index]) {
-        return normalStyle
+      if (ele.disabled) {
+        return this.myNew(normalStyle, disabledStyle)
       } else {
-        return disabledStyle
+        return normalStyle
       }
     }
   }
 
-  onClickTabbar (index) {
-    this.props.onClickTabbar(index)
+  myNew (oldObj, addObj) {
+    let a = JSON.parse(JSON.stringify(oldObj))
+    Object.assign(a, addObj)
+    return a
+  }
+
+  // click
+  onTabClick (ele, index) {
+    console.log('!~!!!')
+    let {onTabClick} = this.props
+    onTabClick && onTabClick(index)
+    this.onChange(ele, index)
+  }
+
+  // change
+  onChange (ele, index) {
+    let {onChange} = this.props
+    if (index !== this.state.currentSelect && !ele.disabled) {
+      console.log('onChange')
+      console.log(index)
+      this.setState({
+        currentSelect: index
+      }, () => {
+        onChange && onChange(index)
+      })
+    }
   }
 }
