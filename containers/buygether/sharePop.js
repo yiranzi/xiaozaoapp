de02d308f3c2574b3d6320b42147f5f1325e9b6c
yiranzi <570932296@qq.com>
2017-11-28 13:39:15 +0800

    update: 重构了buyGether 主要目的是缩减行数，简化逻辑，便于维护。 1、 将纯组件分离出来。尽量都是纯组件（但具有点击事件） 2、分享部分进行逻辑梳理。更加清晰。 3、标题的样式修改成global+伪类 4、将少量的classname和变量名进行调整（还有的有问题没有调整完毕）

---
+++ containers/buygether/sharePop.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import {ModalBoxPopFunc} from '../../xz-components/modalbox'
+/**
+ * 分享弹窗
+ * 传入时间，加上随机的秒
+ * 如果是没有时间，就默认最大。（只有刚开团的自动弹窗没有设置时间）
+ */
+export default function (ele) {
+  let leftHour = 23
+  let leftMinute = 59
+  if (ele) {
+    ({leftHour, leftMinute} = ele)
+  }
+  let defaultStyle = {
+    backgroundColor: 'rgba(0, 10, 49, 0.5)'
+  }
+  let randomSecond = parseInt(60 * Math.random())
+  let dom = <div>
+    <img className='img-style' src='/static/img/buygether/share-arrow.png' />
+    <p className='title'>离成团只剩{leftHour}时{leftMinute}分{randomSecond}秒</p>
+    <p className='title'>还差<strong className='strong'> 1 </strong>人，赶紧邀请好友来拼团吧~</p>
+    <p className='title'>拼团人满后可拿成就卡</p>
+    <style jsx>{`
+      .title {
+        font-size:20px;
+        font-weight: bold;
+      }
+      .strong {
+        font-size:28px;
+        font-weight: bold;
+        color: red;
+      }
+      .img-style {
+        position: absolute;
+        top: 0;
+        right: 0;
+        width: 150px;
+        height: 300px;
+      }
+    `}</style>
+  </div>
+  let prop = {
+    innerDiv: dom,
+    style: defaultStyle
+  }
+  ModalBoxPopFunc({...prop})
+}
+
+
