6f01623481e943a586f630e557301bf385673fea
wangyuanli <yuanli.wang@xiaozao.org>
2017-11-28 18:06:47 +0800

    update: 全局字体调整

--- components/layout.js
+++ components/layout.js
@@ -42,7 +42,7 @@ export default class Layout extends React.Component {
         <script src='/static/js/baidutongji.js' />
         <style global jsx>{`
           body {
-            font-size: 14pt;
+            font-size: 12pt;
             font-family: PingFang SC,Helvetica Neue,Helvetica,Arial,Hiragino Sans GB,Microsoft Yahei,sans-serif !important;
           }
         `}</style>
