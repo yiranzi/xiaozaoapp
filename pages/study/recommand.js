548d187695831b303656ec4bd80ef5665fd6e411
yiranzi <570932296@qq.com>
2017-12-08 19:58:38 +0800

    update:  #9 推荐页开始

---
+++ pages/study/recommand.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '/components/layout'
+export default class extends React.Component {
+  render () {
+    return (
+      <Layout>
+        <div>
+          recommand
+        </div>
+      </Layout>
+    )
+  }
+}
