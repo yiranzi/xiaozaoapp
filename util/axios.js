7a43aab813562930e00c0143e7ac008b7867c127
wangyuanli <yuanli.wang@xiaozao.org>
2017-12-11 15:53:21 +0800

    chore: #22

--- util/axios.js
+++ util/axios.js
@@ -6,28 +6,24 @@ let AxiosUtil = {}
 function request (param) {
   return new Promise((resolve, reject) => {
     axios(param).then((res) => {
-      if (res.status === 200 && res.data.status === 200) {
+      if (res.data.status === 200) {
         resolve(res.data.response)
       } else {
-        if (res.data.status === 9999) {
-          location.href = '/user/register'
-        } else {
-          const {data} = res
-          // 接口返回错误
-          const json = {
-            status: data.status,
-            message: data.message,
-            url: param.url
-          }
-          Tips.info({
-            children: data.message
-          })
-          reject(json)
+        const {data} = res
+        // 接口返回错误
+        const json = {
+          status: data.status,
+          message: data.message,
+          url: param.url
         }
+        Tips.info({
+          children: data.message
+        })
+        reject(json)
       }
     }).catch((error) => {
       if (error.response.status === 401) {
-        // location.reload(true)
+        location.reload(true)
       } else {
         reject(error.message)
       }
