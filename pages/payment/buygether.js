75c1ea6e786c0741a51ea0412baad2311a180368
yiranzi <570932296@qq.com>
2017-12-25 10:10:08 +0800

    update: 分享测试 #34

--- pages/payment/buygether.js
+++ pages/payment/buygether.js
@@ -479,6 +479,8 @@ export default class extends React.Component {
         // 如果是小程序 的上线分享 请修改掉url？ 下线不处理
         // 如果小程序上线开团成功。修改url
         alert(this.littleShareUrl)
+        alert(location.href)
+        location.href = this.littleShareUrl
       }
     }
   }
