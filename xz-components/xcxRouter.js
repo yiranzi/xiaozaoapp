d73063366607935ee029ec365b2f0473bd47fa26
yiranzi <570932296@qq.com>
2017-12-29 20:24:26 +0800

    update: bug fix

--- xz-components/xcxRouter.js
+++ xz-components/xcxRouter.js
@@ -3,12 +3,15 @@ import {Alert} from './alert'
 
 // 根据路径进行跳转
 export function goRouterFunc (pathUrl) {
+  alert('1')
   if (window.__wxjs_environment === 'miniprogram') {
     // 判断链接是否对小程序有效
     let result = pathUrl.search(/xiaozao.org/)
+    alert(result)
     if (result && result !== '-1') {
       Router.push(pathUrl)
     } else {
+      alert('1')
       Alert({ content: '课程正在报名中！获取课程详情、报名课程请联系小助手Harry ( xiaozao906 )' })
     }
   } else {
