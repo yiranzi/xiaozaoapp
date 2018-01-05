4eea5a0d112695657e584d71fc454c3057b68759
yiranzi <570932296@qq.com>
2018-01-05 20:55:13 +0800

    update: 修改homework刷新bug

--- pages/learn/course/otherAnswer.js
+++ pages/learn/course/otherAnswer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Homework from '../../../containers/learn/main/homework/homework'
 import ToolsUtil from '../../../util/tools'
 import Layout from '../../../components/layout'
+import AxiosUtil from '../../../util/axios'
 // import FixFooter from '../../../xz-components/fixfooter'
 
 export default class extends React.Component {
@@ -13,15 +14,18 @@ export default class extends React.Component {
         workId: '',
         type: ''
       },
+      homeWorkData: undefined,
       search: ''
     }
   }
 
-  componentDidMount () {
+  componentDidMount = async () => {
     let courseId = ToolsUtil.getQueryString('courseId')
     let workId = ToolsUtil.getQueryString('workId')
     let type = ToolsUtil.getQueryString('type')
+    let homeWorkData = await AxiosUtil.get(`/api/work/workList/${courseId}`, true)
     this.setState({
+      homeWorkData: homeWorkData,
       query: {
         courseId: courseId,
         workId: workId,
@@ -31,11 +35,11 @@ export default class extends React.Component {
   }
 
   render () {
-    const {query} = this.state
+    const {query, homeWorkData} = this.state
     const {courseId, workId, type} = query
     return (
       <Layout>
-        {courseId && workId && <Homework tabSelect={type} courseStatus={'doing'} courseId={parseInt(courseId)} workId={parseInt(workId)} />}
+        {courseId && workId && <Homework data={homeWorkData} tabSelect={type} courseStatus={'doing'} courseId={parseInt(courseId)} workId={parseInt(workId)} />}
         {/* <FixFooter style={{textAlign: 'center'}} onClick={() => { history.go(-1) }}>继续学习</FixFooter> */}
       </Layout>
     )
