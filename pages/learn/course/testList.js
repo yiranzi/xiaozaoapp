97b0b4147791299bd6969a7a1ccd4136108842b1
wangyuanli <yuanli.wang@xiaozao.org>
2018-01-06 14:37:45 +0800

    fix 测试列表界面根据设计重做

--- pages/learn/course/testList.js
+++ pages/learn/course/testList.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import {Panel, PanelBody, LoadMore} from 'react-weui'
-import classNames from 'classnames'
 import Link from 'next/link'
 import ToolsUtil from '../../../util/tools'
 import AxiosUtil from '../../../util/axios'
 import DataUtil from '../../../util/data'
 import LoadingIcon from '../../../xz-components/loadingicon'
-import Card from '../../../xz-components/card'
 import Layout from '../../../components/layout'
 import Footer from '../../../containers/learn/footer'
 
@@ -58,18 +56,28 @@ export default class extends React.Component {
     }
   }
   renderFinish (item) {
+    let passStyle = {
+      'background': '#fff url(/static/img/learn/test/pass.png) right bottom no-repeat',
+      'background-size': '2.5rem'
+    }
+    let failStyle = {
+      'background': '#fff url(/static/img/learn/test/failed.png) right bottom no-repeat',
+      'background-size': '2.5rem'
+    }
+
+    let style
+    if (item.answerTotalScore >= item.passScore) {
+      style = passStyle
+    } else {
+      style = failStyle
+    }
     return (
-      <div className='test-item'>
+      <div className='test-item' style={style}>
         <div className='wx-space-left'>
-          <div className='icon'><img src='/static/img/icon/prise.png' /></div>
           <div className='wx-space-center' style={{width: '100%'}}>
-            <div className='chapter-title'>{item.title}</div>
+            <div className='chapter-title'><span className='icon' />{item.title}</div>
             <div className='wx-space-left'>
               <div className='score'>{item.answerTotalScore}/{item.score}</div>
-              <div className='stamp'>
-                {item.answerTotalScore >= item.passScore && <img src='/static/img/learn/test/pass.png' />}
-                {item.answerTotalScore < item.passScore && <img src='/static/img/learn/test/fail.png' />}
-              </div>
             </div>
           </div>
         </div>
@@ -77,18 +85,22 @@ export default class extends React.Component {
         <style jsx>{`
           .test-item {
             width: 100%;
-            padding: 0.5rem 2rem;
             box-sizing: border-box;
+            padding: 1rem;
+            margin-top: 0.5rem;
           }
           .icon {
-            margin-right: 1rem;
-          }
-          .icon img {
-            width: 1.5rem;
+            display: inline-block;
+            width: 10px;
+            height: 10px;
+            border-radius: 10px;
+            margin-right: 5px;
+            background-color: #3ea6f7;
           }
           .chapter-description {
-            margin-left: 2.5rem;
             font-size: 0.85rem;
+            margin-top: 0.5rem;
+            color: #646464;
           }
           .stamp img {
             margin-left: 1rem;
@@ -102,9 +114,8 @@ export default class extends React.Component {
     return (
       <div className='test-item'>
         <div className='wx-space-left'>
-          <div className='icon'><img src='/static/img/icon/uprise.png' /></div>
           <div className='wx-space-center' style={{width: '100%'}}>
-            <div className='chapter-title'>{item.title}</div>
+            <div className='chapter-title'><span className='icon' />{item.title}</div>
             <div className='status'>未完成</div>
           </div>
         </div>
@@ -112,21 +123,23 @@ export default class extends React.Component {
         <style jsx>{`
           .test-item {
             width: 100%;
-            padding: 0.5rem 2rem;
             box-sizing: border-box;
+            background-color: #fff;
+            padding: 1rem;
+            margin-top: 0.5rem;
           }
           .icon {
-            margin-right: 1rem;
-          }
-          .icon img {
-            width: 1.5rem;
+            display: inline-block;
+            width: 10px;
+            height: 10px;
+            border-radius: 10px;
+            margin-right: 5px;
+            background-color: #e1e4f0;
           }
           .chapter-description {
-            margin-left: 2.5rem;
             font-size: 0.85rem;
-          }
-          .status {
-            margin-right: 4rem;
+            margin-top: 0.5rem;
+            color: #646464;
           }
         `}</style>
       </div>
@@ -153,6 +166,7 @@ export default class extends React.Component {
                 {group.learningTestDetailDTOList.map((item, index) => {
                   return (
                     <Link
+                      key={`testList-${index}`}
                       href={{
                         pathname: '/learn/course/testDetail',
                         query: {courseId: query.courseId, testId: item.id}}}
@@ -170,6 +184,11 @@ export default class extends React.Component {
             </div>
           )
         })}
+        <style jsx>{`
+          .chapter-item {
+            padding-top: 2rem;
+          }
+        `}</style>
       </div>
     )
   }
@@ -184,7 +203,10 @@ export default class extends React.Component {
         <Footer type='test' />
         <style jsx>{`
           .test-page {
-            padding: 2rem 1rem 4rem 1rem;
+            padding: 0 1rem 4rem 1rem;
+            min-height: 100vh;
+            box-sizing: border-box;
+            background-color: #EFEFF4;
           }
         `}</style>
       </Layout>
