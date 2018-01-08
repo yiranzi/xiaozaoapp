0a75c571ca5d38842103f43417c9245b3d7a3e07
wangyuanli <yuanli.wang@xiaozao.org>
2018-01-08 16:31:47 +0800

    fix 课程介绍识别文本标签

--- containers/learn/main/introduce.js
+++ containers/learn/main/introduce.js
@@ -7,29 +7,33 @@ class innerComponent extends React.Component {
   render () {
     let {data} = this.props
     if (data && data.length > 0) {
-      return (<div className='introduce-div'>
-        {data.map((ele, index) => {
-          return (<div className='introduce-section' key={index}>
-            <h1>{ele.title}</h1>
-            <p className='content'>{ele.content}</p>
-          </div>)
-        })}
-        <style jsx>{`
-        .introduce-div {
-          text-align: left;
-          padding: 10px;
-        }
-        .introduce-section {
-          margin: 10px auto;
-        }
-        .introduce-section h1 {
-          font-size: 16px;
-        }
-        .introduce-section .content {
-          font-size: 14px;
-        }
-      `}</style>
-      </div>)
+      return (
+        <div className='introduce-div'>
+          {data.map((ele, index) => {
+            return (
+              <div className='introduce-section' key={index}>
+                <h1>{ele.title}</h1>
+                <div className='content' dangerouslySetInnerHTML={{__html: ele.content}} />
+              </div>
+            )
+          })}
+          <style jsx>{`
+          .introduce-div {
+            text-align: left;
+            padding: 10px;
+          }
+          .introduce-section {
+            margin: 10px auto;
+          }
+          .introduce-section h1 {
+            font-size: 16px;
+          }
+          .introduce-section .content {
+            font-size: 14px;
+          }
+        `}</style>
+        </div>
+      )
     } else {
       return (
         <Panel className='introduce'>
